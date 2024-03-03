60e3b982cc90e47cd4f223ac62d478fe5eb84fd6
mmkaram <mahdykaram@outlook.com>
2024-03-02 20:41:31 -0800

    works

--- Final/final_venv/Scripts/activate.bat
+++ Final/final_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Prog\ATC-W-24\AdvTopics-W-23\Final\final_venv
+set VIRTUAL_ENV=C:\Prog\AdvTopics-W-23\Final\final_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
